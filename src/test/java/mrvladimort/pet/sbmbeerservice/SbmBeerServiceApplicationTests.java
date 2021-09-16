a24e0818840142aa502f847222b559d07e141053
Vladyslav Hravchenko <mrvladimort@gmail.com>
2021-09-16 23:22:09 +0300

    Initial structure

---
+++ src/test/java/mrvladimort/pet/sbmbeerservice/SbmBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mrvladimort.pet.sbmbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
